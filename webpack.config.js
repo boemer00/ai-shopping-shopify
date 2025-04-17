79f3f9c9b75e7196530b97c19856471af8062451
renato boemer <boemer00@gmail.com>
2025-04-17 15:17:54 +0100

    fixed: variable error in webpack build and improve embed implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -24,6 +25,22 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
+  },
+  plugins: [
+    // Define process.env so it's available in browser
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+      'process.browser': JSON.stringify(true)
+    }),
+    // Provide process globally
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
+  ],
+  resolve: {
+    fallback: {
+      "process": require.resolve("process/browser")
+    }
   },
   devServer: {
     static: {
